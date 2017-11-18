a2581f221a465ce13ea5cb849c16e0b2ce0366f3
Lars Ole Simonsen <los@c3a.dk>
2017-11-18 22:00:05 +0100

    Change the App component into the ts class format

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,17 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
 import HelloWorld from './components/HelloWorld'
 
-export default Vue.extend({
-  name: 'app',
+@Component({
   components: {
     HelloWorld
   }
 })
+export default class App extends Vue {
+
+}
 </script>
 
 <style lang="scss">
